710705944c54e8d364b6b155ed75de2fb66de268
Dale Nacianceno <10011658@sbstudents.org>
2018-10-01 08:12:05 -0400

    Add files via upload

--- main2.java
+++ main2.java
@@ -10,14 +10,14 @@ import java.math.BigInteger;
 public class main2 {
 
 	static File name = new File("input2.txt");
-	static ArrayList<Integer> list = new ArrayList<Integer>();
+	static ArrayList<BigInteger> list = new ArrayList<BigInteger>();
 
-	public static BigInteger lucas(Integer count) {
+	public static BigInteger lucas(BigInteger count) {
 		BigInteger x = BigInteger.valueOf(2);
 		BigInteger y = BigInteger.valueOf(1);
 		BigInteger temp;
 		BigInteger tempY;
-		for (int i=0;i<count;i++) {
+		for (BigInteger i = BigInteger.ZERO; i.compareTo(count) < 0; i = i.add(BigInteger.ONE)) {
 			temp = new BigInteger(y+"");
 			tempY = new BigInteger(y+"");
 			y = new BigInteger((tempY.add(x))+"");
@@ -32,9 +32,9 @@ public class main2 {
 			BufferedReader input = new BufferedReader(new FileReader(name));
 			String text, output = "";
 			while((text=input.readLine())!=null) {
-				list.add(Integer.parseInt(text));
+				list.add(new BigInteger(text));
 			}
-			for (Integer i : list) {
+			for (BigInteger i : list) {
 				System.out.println(lucas(i));
 			}
 		} catch (IOException io) {
